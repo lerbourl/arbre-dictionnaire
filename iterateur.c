555c635e97b85bce49243d9eae8c041d44597d1d
lerbourl <louis.lerbourg@gmail.com>
2018-11-27 16:23:35 +0100

    debut it

---
+++ iterateur.c
@@ -0,0 +1,18 @@
+#include "dico.h"
+#include "iterateur.h"
+
+iterator * start_iterator(dico d){
+    return;
+}
+
+void destroy_iterator(iterator ** it){
+
+}
+
+bool has_next(iterator * it){
+    return;
+}
+
+char * next (iterator * it){
+    return;
+}
---
+++ iterateur.h
@@ -0,0 +1,26 @@
+#ifndef ITERATEUR_H
+#define ITERATEUR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+struct iterator_info {
+    tree t;
+    int index_word;
+};
+
+struct _iterator {
+    char * word;
+    struct iterator_info * stack;
+    int index_stack;
+};
+
+typedef struct _iterator iterator;
+
+iterator * start_iterator(dico d);
+void destroy_iterator(iterator ** it);
+bool has_next(iterator * it);
+char * next (iterator * it);
+
+#endif
